39d9d0a6f5323c155a0d08d1e54a37f6da99c619
PE8RI <ritwickraj5632@gmail.com>
2025-09-02 01:57:32 +0530

    fix: correct event delegation by using nodeName in uppercase for delete button

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ btn.addEventListener("click",function(){
   inp.value="";
 });
 
-let delBtns=document.querySelectorAll(".delete");
-for(delBtn of delBtns){
-  delBtn.addEventListener("click",function(){
-    let par = this.parentElement;
-    par.remove();
-  });
-}
+ 
+ul.addEventListener('click',function(event){
+ if(event.target.nodeName=='BUTTON'){
+    let listItem=event.target.parentElement;
+      listItem.remove();
+ }
+});
